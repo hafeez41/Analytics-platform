888d324f65da8ab6df714ab04e4aee764a8f5c03
Abdulhafiz abubakar <87665336+hafeez41@users.noreply.github.com>
2025-08-24 14:50:42 +0100

    Clean

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,7 +1,3 @@
 // Database connection setup
-// We'll add the actual database client in Step 2, but this prevents the import error
-
-// Database connection setup
-// We'll add the actual database client in Step 2, but this prevents the import error
 
 export const placeholderr = 'Database client will be defined here';
--- packages/db/src/schema.ts
+++ packages/db/src/schema.ts
@@ -1,4 +1,3 @@
 // Database table definitions
-// We'll add the actual tables in Step 2, but this prevents the import error
 
 export const placeholder = 'Database schema will be defined here';
